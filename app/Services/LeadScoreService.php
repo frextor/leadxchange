b29601403644689549660ed623c4607ec8f73981
anass <anass.alouane@gmail.com>
2026-06-24 14:27:37 +0100

    revert: remove rating score clamp, score can be negative

--- app/Services/LeadScoreService.php
+++ app/Services/LeadScoreService.php
@@ -71,7 +71,7 @@ class LeadScoreService
 
         $receivedPoints = $receivedCount * self::RECEIVED_MALUS;
 
-        return max(0, $sentPoints + $receivedPoints);
+        return $sentPoints + $receivedPoints;
     }
 
     public function badge(int $score): string
